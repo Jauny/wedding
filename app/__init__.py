e554e3ded8cff3b2bb1459d318fe9a81b03c2fa5
Jonathan Pepin <jypepin@gmail.com>
2018-02-18 17:32:35 +0100

    some logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+
+import logging
+# setup logging https://docs.python.org/2/howto/logging.html#a-simple-example
+
 app = Flask(__name__)
 
 app.config.from_pyfile('config.cfg')
+config = app.config
+logger = app.logger
 
 from app import views
--- app/helpers/email.py
+++ app/helpers/email.py
@@ -1,19 +1,30 @@
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
-import os
-
+from app import config, logger
 import sendgrid
 from sendgrid.helpers.mail import *
 
-import pdb; pdb.set_trace();
+sg = sendgrid.SendGridAPIClient(apikey=config.get('SENDGRID_API_KEY'))
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("test@example.com")
-to_email = Email("test@example.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+FROM_EMAIL = 'jypepin@gmail.com'
+
+def send_email(to_email, subject, html_body):
+    """Send email to to_email, with subject and body."""
+    logger.info('sending email', to_email=to_email, subject=subject)
+
+    mail = Mail(FROM_EMAIL, subject, to_email,
+                Content("text/html", html_body))
+
+    response = sg.client.mail.send.post(request_body=mail.get())
+
+    logger.info('sent email', to_email=to_email, subject=subject)
+
+# from_email = Email("test@example.com")
+# to_email = Email("test@example.com")
+# subject = "Sending with SendGrid is Fun"
+# content = Content("text/plain", "and easy to do anywhere, even with Python")
+# mail = Mail(from_email, subject, to_email, content)
+# response = sg.client.mail.send.post(request_body=mail.get())
+# print(response.status_code)
+# print(response.body)
+# print(response.headers)
