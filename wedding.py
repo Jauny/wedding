06d2ad3fd44c3b13ac1e0eef4c6deb87306d54f4
Jonathan Pepin <jonathanp@uber.com>
2018-02-22 12:16:55 +0100

    add python-dotenv and cleanup startup

--- wedding.py
+++ wedding.py
@@ -1,11 +1,14 @@
 import os
+
+from dotenv import load_dotenv
 from app import app
 
 
-#----------------------------------------
-# launch
-#----------------------------------------
-
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host='0.0.0.0', port=port)
+    if os.environ.get('ENVIRONMENT') is not 'PRODUCTION':
+        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+
+    import pdb; pdb.set_trace()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
