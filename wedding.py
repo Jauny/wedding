d5ab8d4f8828ab3e22da497d3a85edd3bfc1218c
Jonathan Pepin <jonathanp@uber.com>
2018-02-22 12:26:28 +0100

    remove pdb

--- wedding.py
+++ wedding.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
         dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
         load_dotenv(dotenv_path)
 
-    import pdb; pdb.set_trace()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
