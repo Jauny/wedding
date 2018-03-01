dabfd9a60905d1f8dbe24795cb6f91179cb6b3c3
Jonathan Pepin <jonathanp@uber.com>
2018-03-01 13:34:12 +0100

    add invites model and plug it with airtable and the rsvp form

---
+++ app/models/invites.py
@@ -0,0 +1,98 @@
+import json
+import requests
+
+from app.helpers import airtable
+
+
+class Invite:
+    """Invite model, backed up on Airtable 'invites' table."""
+    base_url = '{base_url}/invites{api_key}'.format(
+        base_url=airtable.BASE_URL,
+        api_key=airtable.API_KEY)
+
+    base_url_post = '{base_url}/invites/{{id}}{api_key}'.format(
+        base_url=airtable.BASE_URL,
+        api_key=airtable.API_KEY)
+
+    def __init__(self, **kwargs):
+        """Init an invite from kwargs."""
+        if kwargs:
+            for key, value in kwargs.iteritems():
+                setattr(self, key, value)
+
+    @classmethod
+    def getAll(cls):
+        """Return all invites from airtable."""
+        resp = requests.get(cls.base_url)
+        return json.loads(resp.read()).get('records')
+
+    @classmethod
+    def getAllUnsent(cls):
+        """Return all unsent invites."""
+        data = {
+            'filterByFormula': 'NOT({sent})'
+        }
+        resp = requests.get(
+            cls.base_url,
+            params=data)
+        return resp.json().get('records')
+
+    @classmethod
+    def getAllMissingRSVP(cls, offset=None):
+        """Return all unsent invites."""
+        results = []
+
+        data = {'filterByFormula': '{sent} = ""'}
+        if offset:
+            data['offset'] = offset
+
+        resp = requests.get(cls.base_url, params=data)
+        data = resp.json()
+
+        results += data.get('records')
+        while data.get('offset'):
+            results += cls.getAllMissingRSVP(cls, offset)
+
+        return results
+
+    @classmethod
+    def getFromEmail(cls, email):
+        """Return record with email."""
+        data = {'filterByFormula': '{email} = "%s"' % email}
+
+        resp = requests.get(cls.base_url, params=data)
+        records = resp.json().get('records')
+        if len(records):
+            return records[0]
+        else:
+            return None
+
+    @classmethod
+    def getPlusoneFromInvite(cls, invite):
+        """Return plusone invite from invite or None."""
+        plusone = invite.get('fields', {}).get('plusone')
+        if plusone:
+            return cls.getFromEmail(plusone)
+
+        return None
+
+    @classmethod
+    def updateRSVPForEmail(cls, email, rsvp, rehearsal):
+        """Update invite for email with rsvp."""
+        invite = cls.getFromEmail(email)
+        if not invite:
+            return None
+
+        url = cls.base_url_post.format(id=invite.get('id'))
+        data = {
+            'fields': {
+                'rsvp': rsvp,
+                'rehearsal': rehearsal
+            }
+        }
+
+        resp = requests.patch(
+            url=url,
+            data=json.dumps(data),
+            headers={'Content-Type': 'application/json'})
+        return resp.json()
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import logging
 from flask import redirect, render_template, request, url_for
 
 from app import app
-# from app.models.invites import Invite
+from app.models.invites import Invite
 
 log = logging.getLogger(__name__)
 
@@ -19,34 +19,45 @@ def rsvp():
         return render_template('rsvp.html')
 
     # find invitation for email
-    # email = request.form.get('email')
-    # invite = Invite.getFromEmail(email)
-    invite = None
+    email = request.form.get('email')
+    invite = Invite.getFromEmail(email)
 
     if not invite:
         return render_template(
             'rsvp.html',
             error='We don\'t have this email. Do you have another email?')
 
-    return render_template('rsvp-confirm.html', invite=invite)
+    plusone = Invite.getPlusoneFromInvite(invite)
+
+    return render_template(
+        'rsvp-confirm.html',
+        invite=invite.get('fields'),
+        plusone=plusone.get('fields') if plusone else None)
 
 
 @app.route('/rsvp_confirm', methods=['GET', 'POST'])
 def rsvp_confirm():
     """Confirm RSVP with details."""
     if request.method == 'GET':
-        return render_template(
-            'rsvp-confirm.html',
-            name='Jonathan',
-            plusone='Jessica')
-        # return redirect(url_for('rsvp'))
+        return redirect(url_for('rsvp'))
 
+    email = request.form.get('email')
     rsvp = request.form.get('rsvp')
-    plusone = request.form.get('plusone')
+    plusone_email = request.form.get('plusone_email')
+    plusone_rsvp = request.form.get('plusone_rsvp')
     rehearsal = request.form.get('rehearsal')
 
     # update rsvp invite
+    Invite.updateRSVPForEmail(email, rsvp, rehearsal)
+
     # update plusone invite
+    if plusone_email:
+        if plusone_rsvp == 'no' or rehearsal == 'no':
+            plusone_rehearsal = 'no'
+        else:
+            plusone_rehearsal = 'yes'
+        Invite.updateRSVPForEmail(
+            plusone_email, plusone_rsvp, plusone_rehearsal)
 
     if rsvp == 'yes':
         message = 'Hooray! We will see you in Palm Springs.'
